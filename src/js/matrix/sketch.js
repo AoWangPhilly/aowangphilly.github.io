b27ab65c520fe882aedd5d447233fa378bce300e
AoWangDrexel <aw3338@drexel.edu>
2020-07-27 00:37:50 -0400

    Create streams of matrix rain

--- src/js/matrix/sketch.js
+++ src/js/matrix/sketch.js
@@ -1,20 +1,25 @@
-let symbol;
-let symbolSize = 30;
+let streams = [];
+let symbolSize = 20;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
-    symbol = new Matrix(
-        windowWidth / 2,
-        0,
-        random(5, 10));
-    symbol.setToRandomSymbol();
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < (windowWidth / symbolSize) + 1; i++) {
+        let stream = new Stream();
+        stream.generateSymbols(x, y);
+        streams.push(stream);
+        x += symbolSize;
+    }
     textSize(symbolSize);
 }
 
 function draw() {
     background(0);
-    symbol.render();
+    streams.forEach(stream => {
+        stream.render();
+    });
 }
 
 function windowResized() {
@@ -28,27 +33,47 @@ function Matrix(x, y, spd) {
     this.y = y;
     this.spd = spd;
     this.value;
+    this.switchInterval = round(random(2, 20));
 }
 
 Matrix.prototype = {
     constructor: Matrix,
     setToRandomSymbol: function () {
-        this.value = String.fromCharCode(
-            0x30A0 + floor(random(97))
-        );
+        if (frameCount % this.switchInterval == 0) {
+            this.value = String.fromCharCode(
+                0x30A0 + floor(random(97))
+            );
+        }
     },
     rain: function () {
-        this.y += this.spd;
-    },
-    render: function () {
-        fill(0, 255, 70);
-        text(this.value, this.x, this.y);
-        this.rain();
+        this.y = this.y >= windowHeight ? 0 : this.y += this.spd;
     }
 }
 
 
 // Stream class
 function Stream() {
+    this.symbols = [];
+    this.totalSymbols = round(random(5, 30));
+    this.spd = round(random(5, 10));
+}
 
+Stream.prototype = {
+    constructor: Stream,
+    generateSymbols: function (x, y) {
+        for (let i = 0; i < this.totalSymbols + 1; i++) {
+            let symbol = new Matrix(x, y, this.spd);
+            symbol.setToRandomSymbol();
+            this.symbols.push(symbol);
+            y -= symbolSize;
+        }
+    },
+    render: function () {
+        this.symbols.forEach(symbol => {
+            fill(0, 255, 70);
+            text(symbol.value, symbol.x, symbol.y);
+            symbol.rain();
+            symbol.setToRandomSymbol();
+        })
+    }
 }
